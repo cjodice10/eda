79b096961df9343f29178a068ff9c2351b0c80d6
Jodice Family <jodicefamily@Jodices-MBP.fios-router.home>
2020-12-04 16:05:59 -0500

    na values udpate

--- R/get_categorical_bins.R
+++ R/get_categorical_bins.R
@@ -451,13 +451,16 @@ get_categorical_bins<-function(  df
     data.frame();
   CategoricalEDA.fine = CategoricalEDA.fine[,c("Variable","bin_id","Records","Events","EventRate","WOE","GRP")]
 
+
+  message("After strsplit and collapsing:");print(CategoricalEDA.fine)
+
   CategoricalEDA.fine = CategoricalEDA.fine %>%
     dplyr::group_by(Variable) %>%
     dplyr::mutate(GRP = dplyr::row_number()) %>%
     data.frame();
 
 
-  CategoricalEDA.fine$GRP= ifelse(is.na(CategoricalEDA.fine$bin_id),-1,CategoricalEDA.fine$GRP)
+  #CategoricalEDA.fine$GRP= ifelse(is.na(CategoricalEDA.fine$bin_id),-1,CategoricalEDA.fine$GRP)
   CategoricalEDA.fine    = CategoricalEDA.fine[order(CategoricalEDA.fine$Variable,CategoricalEDA.fine$GRP),]
 
   #loop through each avariable
